392777514ed868fb8fad224b9bd250931294a16b
graveyardkeeper <naughty_dog@qq.com>
2024-12-21 18:26:44 +0800

    Goblin slingshot attack animation

--- Source/Aura/Public/Character/AuraCharacterBase.h
+++ Source/Aura/Public/Character/AuraCharacterBase.h
@@ -46,7 +46,7 @@ protected:
 
 	virtual void InitializeDefaultAttributes() const;
 
-	UPROPERTY(EditAnywhere, Category="Combat")
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="Combat")
 	TObjectPtr<USkeletalMeshComponent> Weapon;
 
 	UPROPERTY(EditAnywhere, Category="Combat")
