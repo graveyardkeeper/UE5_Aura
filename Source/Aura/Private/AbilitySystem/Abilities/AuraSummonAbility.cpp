48d04ee5ac3ed5dfbef75b5eb62f418bda32a9c1
graveyardkeeper <naughty_dog@qq.com>
2024-12-23 23:47:46 +0800

    Spawn locations in spread

---
+++ Source/Aura/Private/AbilitySystem/Abilities/AuraSummonAbility.cpp
@@ -0,0 +1,41 @@
+// There is no fucking Copyright
+
+
+#include "AbilitySystem/Abilities/AuraSummonAbility.h"
+#include "Aura/Aura.h"
+#include "Kismet/KismetSystemLibrary.h"
+
+TArray<FVector> UAuraSummonAbility::GetSpawnLocations()
+{
+	const FVector Location = GetAvatarActorFromActorInfo()->GetActorLocation();
+	const FVector Forward = GetAvatarActorFromActorInfo()->GetActorForwardVector();
+
+	// 从左到右顺时针旋转，初始往逆时针方向旋转SpawnSpread一半的角度，之后每次旋转DeltaAngle个角度
+	FVector StartOfSpread = Forward.RotateAngleAxis(-SpawnSpread / 2.f, FVector::UpVector);
+	float DeltaAngle = 0.f;
+	if (NumMinions > 1)
+	{
+		DeltaAngle = SpawnSpread / (NumMinions - 1); // 这里，需要-1，因为是生成在线上，n条线只用旋转n-1次
+	}
+	else
+	{
+		StartOfSpread = Forward; // 只有一条线，使用Forward，for中的旋转无效
+	}
+
+	TArray<FVector> SpawnLocations;
+	for (int32 i = 0; i < NumMinions; i++)
+	{
+		const FVector Direction = StartOfSpread.RotateAngleAxis(i * DeltaAngle, FVector::UpVector);
+		const FVector SpawnLocation = Location + Direction * FMath::RandRange(MinSpawnDistance, MaxSpawnDistance);
+		SpawnLocations.Add(SpawnLocation);
+#ifdef AURA_DEBUG
+		UKismetSystemLibrary::DrawDebugArrow(GetWorld(), Location, Location + Direction * MaxSpawnDistance, 4.f,
+		                                     FLinearColor::Red, 2.f);
+		DrawDebugSphere(GetWorld(), Location + Direction * MinSpawnDistance, 5.f, 12, FColor::White, false, 2.f);
+		DrawDebugSphere(GetWorld(), Location + Direction * MaxSpawnDistance, 5.f, 12, FColor::White, false, 2.f);
+		DrawDebugSphere(GetWorld(), SpawnLocation, 10.f, 12, FColor::Green, false, 2.f);
+#endif
+	}
+
+	return SpawnLocations;
+}
---
+++ Source/Aura/Public/AbilitySystem/Abilities/AuraSummonAbility.h
@@ -0,0 +1,36 @@
+// There is no fucking Copyright
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AbilitySystem/Abilities/AuraGameplayAbility.h"
+#include "AuraSummonAbility.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API UAuraSummonAbility : public UAuraGameplayAbility
+{
+	GENERATED_BODY()
+
+public:
+	UFUNCTION(BlueprintCallable)
+	TArray<FVector> GetSpawnLocations();
+
+protected:
+	UPROPERTY(EditDefaultsOnly, Category="Summoning")
+	TArray<TSubclassOf<APawn>> MinionClasses;
+
+	UPROPERTY(EditDefaultsOnly, Category="Summoning")
+	int32 NumMinions = 1;
+
+	UPROPERTY(EditDefaultsOnly, Category="Summoning")
+	float MinSpawnDistance = 100.f;
+
+	UPROPERTY(EditDefaultsOnly, Category="Summoning")
+	float MaxSpawnDistance = 400.f;
+
+	UPROPERTY(EditDefaultsOnly, Category="Summoning")
+	float SpawnSpread = 90.f; // 扇出角度
+};
