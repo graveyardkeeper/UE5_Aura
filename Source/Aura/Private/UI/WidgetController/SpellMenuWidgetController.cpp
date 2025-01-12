b242ff3ee232336c80583994641b8d2f9f9cdaa8
graveyardkeeper <naughty_dog@qq.com>
2025-01-12 13:12:32 +0800

    Set Spell Menu Buttons Enable Statuses

--- Source/Aura/Private/UI/WidgetController/SpellMenuWidgetController.cpp
+++ Source/Aura/Private/UI/WidgetController/SpellMenuWidgetController.cpp
@@ -3,6 +3,10 @@
 
 #include "UI/WidgetController/SpellMenuWidgetController.h"
 
+#include "AuraGameplayTags.h"
+#include "AbilitySystem/AuraAbilitySystemComponent.h"
+#include "Player/AuraPlayerState.h"
+
 void USpellMenuWidgetController::BindCallbacksToDependencies()
 {
 	Super::BindCallbacksToDependencies();
@@ -11,4 +15,49 @@ void USpellMenuWidgetController::BindCallbacksToDependencies()
 void USpellMenuWidgetController::BroadcastInitialValues()
 {
 	Super::BroadcastInitialValues();
+}
+
+void USpellMenuWidgetController::SpellSelected(const FGameplayTag& AbilityTag)
+{
+	FGameplayTag StatusTag = FAuraGameplayTags::Get().Ability_Status_Locked;
+
+	if (AbilityTag.IsValid())
+	{
+		if (const FGameplayAbilitySpec* Spec = GetAuraASC()->GetSpecFromAbilityTag(AbilityTag))
+		{
+			StatusTag = GetAuraASC()->GetAbilityStatusFromSpec(*Spec);
+		}
+	}
+
+	bool bShouldEnableSpendPoints = false;
+	bool bShouldEnableEquipSpell = false;
+	ShouldEnableButtons(StatusTag, GetAuraPS()->GetPlayerSpellPoints(), bShouldEnableSpendPoints, bShouldEnableEquipSpell);
+
+	OnSpellSelectedDelegate.Broadcast(bShouldEnableSpendPoints, bShouldEnableEquipSpell);
+}
+
+void USpellMenuWidgetController::ShouldEnableButtons(const FGameplayTag& StatusTag, int32 SpellPoints, bool& bShouldEnableSpendPoints, bool& bShouldEnableEquipSpell)
+{
+	const FAuraGameplayTags& AuraTags = FAuraGameplayTags::Get();
+
+	if (StatusTag.MatchesTagExact(AuraTags.Ability_Status_Locked))
+	{
+		bShouldEnableSpendPoints = false;
+		bShouldEnableEquipSpell = false;
+	}
+	else if (StatusTag.MatchesTagExact(AuraTags.Ability_Status_Eligible))
+	{
+		bShouldEnableSpendPoints = SpellPoints > 0;
+		bShouldEnableEquipSpell = false;
+	}
+	else if (StatusTag.MatchesTagExact(AuraTags.Ability_Status_Unlocked))
+	{
+		bShouldEnableSpendPoints = SpellPoints > 0;
+		bShouldEnableEquipSpell = true;
+	}
+	else if (StatusTag.MatchesTagExact(AuraTags.Ability_Status_Equipped))
+	{
+		bShouldEnableSpendPoints = SpellPoints > 0;
+		bShouldEnableEquipSpell = true;
+	}
 }
--- Source/Aura/Public/UI/WidgetController/SpellMenuWidgetController.h
+++ Source/Aura/Public/UI/WidgetController/SpellMenuWidgetController.h
@@ -7,6 +7,8 @@
 #include "UI/WidgetController/AuraWidgetController.h"
 #include "SpellMenuWidgetController.generated.h"
 
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_TwoParams(FOnSpellSelectedDelegate, bool, bEnableSpendPoints, bool, bEnableEquipSpell);
+
 /**
  * 
  */
@@ -16,6 +18,16 @@ class AURA_API USpellMenuWidgetController : public UAbilityInfoBasedWidgetContro
 	GENERATED_BODY()
 
 public:
+	UPROPERTY(BlueprintAssignable, Category="GAS|Abilities")
+	FOnSpellSelectedDelegate OnSpellSelectedDelegate;
+
 	virtual void BindCallbacksToDependencies() override;
 	virtual void BroadcastInitialValues() override;
+
+	/** 技能图标被选中时触发 */
+	UFUNCTION(BlueprintCallable, Category="GAS|Abilities")
+	void SpellSelected(const FGameplayTag& AbilityTag);
+
+protected:
+	static void ShouldEnableButtons(const FGameplayTag& StatusTag, int32 SpellPoints, bool& bShouldEnableSpendPoints, bool& bShouldEnableEquipSpell);
 };
