7557547c08da8116336b42df6a4b2e5aef0232fe
graveyardkeeper <naughty_dog@qq.com>
2025-02-06 22:38:08 +0800

    Magic Circle

---
+++ Source/Aura/Private/Actor/MagicCircle.cpp
@@ -0,0 +1,24 @@
+// There is no fucking Copyright
+
+
+#include "Actor/MagicCircle.h"
+
+#include "Components/DecalComponent.h"
+
+AMagicCircle::AMagicCircle()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+	MagicCircleDecal = CreateDefaultSubobject<UDecalComponent>("MagicCircleDecalComponent");
+	MagicCircleDecal->SetupAttachment(GetRootComponent());
+}
+
+void AMagicCircle::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+void AMagicCircle::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
