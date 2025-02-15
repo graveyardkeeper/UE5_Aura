fcba81ccd0149887a387c16f2753dc485332194d
graveyardkeeper <naughty_dog@qq.com>
2025-02-15 23:44:59 +0800

    Spawning Fire Balls

--- Source/Aura/Private/AbilitySystem/Abilities/AuraFireBlast.cpp
+++ Source/Aura/Private/AbilitySystem/Abilities/AuraFireBlast.cpp
@@ -3,6 +3,10 @@
 
 #include "AbilitySystem/Abilities/AuraFireBlast.h"
 
+#include "AbilitySystem/AuraAbilitySystemLibrary.h"
+#include "Actor/AuraFireBall.h"
+#include "Kismet/KismetSystemLibrary.h"
+
 FString UAuraFireBlast::GetDescription(int32 Level)
 {
 	const int32 DamageAmount = Damage.GetValueAtLevel(Level);
@@ -47,5 +51,26 @@ FString UAuraFireBlast::GetNextLevelDescription(int32 Level)
 
 TArray<AAuraFireBall*> UAuraFireBlast::SpawnFireBalls()
 {
-	return {};
+	const FVector Forward = GetAvatarActorFromActorInfo()->GetActorForwardVector();
+	TArray<FVector> SpawnDirections = UAuraAbilitySystemLibrary::EvenlyRotatedVectors(Forward, FVector::UpVector, 360.f, NumFireBalls);
+
+	TArray<AAuraFireBall*> FireBallsSpawned;
+	for (const FVector& Direction : SpawnDirections)
+	{
+		FTransform SpawnTransform;
+		SpawnTransform.SetLocation(GetAvatarActorFromActorInfo()->GetActorLocation());
+		SpawnTransform.SetRotation(Direction.Rotation().Quaternion());
+		AAuraFireBall* FireBall = GetWorld()->SpawnActorDeferred<AAuraFireBall>(
+			FireBallClass,
+			SpawnTransform,
+			GetAvatarActorFromActorInfo(),
+			GetCurrentActorInfo()->PlayerController->GetPawn(),
+			ESpawnActorCollisionHandlingMethod::AlwaysSpawn);
+
+		FireBall->DamageEffectParams = MakeDamageParamsFromClassDefaults();
+
+		FireBall->FinishSpawning(SpawnTransform);
+		FireBallsSpawned.Add(FireBall);
+	}
+	return FireBallsSpawned;
 }
