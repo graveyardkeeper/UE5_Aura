9978c1ccaa823ce63f25ffbe0fb0e8db04debf8d
graveyardkeeper <naughty_dog@qq.com>
2025-02-26 00:15:21 +0800

    Checkpoints

---
+++ Source/Aura/Public/Checkpoint/Checkpoint.h
@@ -0,0 +1,39 @@
+// There is no fucking Copyright
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerStart.h"
+#include "Checkpoint.generated.h"
+
+class USphereComponent;
+/**
+ * 
+ */
+UCLASS()
+class AURA_API ACheckpoint : public APlayerStart
+{
+	GENERATED_BODY()
+
+public:
+	ACheckpoint(const FObjectInitializer& ObjectInitializer);
+
+protected:
+	virtual void BeginPlay() override;
+
+	UFUNCTION()
+	virtual void OnSphereOverlap(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp,
+	                             int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+	UFUNCTION(BlueprintImplementableEvent)
+	void CheckpointReached(UMaterialInstanceDynamic* DynamicMaterialInstance);
+
+	void HandleGlowEffects();
+
+private:
+	UPROPERTY(VisibleAnywhere)
+	TObjectPtr<UStaticMeshComponent> CheckpointMesh;
+
+	UPROPERTY(VisibleAnywhere)
+	TObjectPtr<USphereComponent> Sphere;
+};
